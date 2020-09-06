fd94d6137cdde6dab2c1c782b978bf5c41877898
usac201701026 <66382330+usac201701026@users.noreply.github.com>
2020-09-06 16:32:48 -0600

    Add files via upload

---
+++ display_on/display.vhd
@@ -0,0 +1,30 @@
+----------------------------------------------------------------------------------
+
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity display is
+    Port ( Clk : in  STD_LOGIC;
+          Enable : out  STD_LOGIC;
+          Display : out  STD_LOGIC_VECTOR (7 downto 0);
+           b : in  STD_LOGIC);
+end display;
+
+architecture Behavioral of display is
+begin
+Enable <= '0';
+process( b)
+begin
+if(rising_edge(Clk))then
+	if(b='0')then
+		Display <= "11111001";
+		else 
+		Display <= "10100100";
+	end if;
+end if;
+end process;
+end Behavioral;
+
