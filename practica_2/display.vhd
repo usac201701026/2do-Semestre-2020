fbe2ef1e93cabed3f5574d7389cbdb02980dfa19
usac201701026 <66382330+usac201701026@users.noreply.github.com>
2020-09-05 23:20:32 -0600

    Add files via upload

---
+++ practica_2/display.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    20:38:37 09/05/2020 
+
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+
+entity display is
+port(
+	sele : in std_logic;
+	CAR : out std_logic_vector(6 downto 0);
+	disp : out std_logic_vector(2 downto 0)
+--	sw_1, sw_2 : in std_logic
+	
+);
+end display;
+
+architecture Behavioral of display is
+
+begin
+	-- select = 0 dejo pasar data 1 a led
+	-- si selec = 1 dejo pasar data_2 a led
+process(sele)
+	begin 
+	if sele = '1' then
+		CAR <= "1001111";
+		disp <= "000";
+	else 
+		CAR <= "1001111";
+		disp <= "000";
+	end if;
+end process;
+		
+	
+
+
+end Behavioral;
+
---
+++ practica_2/mux.vhd
@@ -0,0 +1,26 @@
+----------------------------------------------------------------------------------
+
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity mux is
+port(
+--	sel : in std_logic;
+	data_1, data_2 : in std_logic_vector(7 downto 0);
+	led : out std_logic_vector(7 downto 0);
+	sw_1, sw_2 : in std_logic
+	
+);
+end mux;
+
+architecture Behavioral of mux is
+begin
+	-- select = 0 dejo pasar data 1 a led
+	-- si selec = 1 dejo pasar data_2 a led
+	led <= data_1 when sw_1 = '0' else 
+			 data_2 when sw_2 = '0' else 
+			 "11111111" when sw_1 = '1' and sw_2 = '1';			 
+end Behavioral;
